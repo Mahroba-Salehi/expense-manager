1416e559158d68b0937572a2f7239dcebd4718b5
mahroba <mahroba.slh@gmail.com>
2025-09-29 23:03:27 +0330

    UPDATE

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from model.entity import *
 from tools.logging import Logger
-from view.main_view import Logger
 from view.main_view import MainView
 import tkinter as tk
 Logger.info("application started")
-if name =="main":
+if __name__ =="__main__":
     root = tk.Tk()
     app = MainView(root)
     root.mainloop()
